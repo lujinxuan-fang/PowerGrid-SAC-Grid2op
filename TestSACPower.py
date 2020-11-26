23cc8988e44fca2820d3b11712f5b8dbb4b3a321
lujinxuan-fang <57823348+lujinxuan-fang@users.noreply.github.com>
2020-11-26 14:34:44 +0800

    SAC - V1

---
+++ TestSACPower.py
@@ -0,0 +1,317 @@
+import math
+import random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+from grid2op.Agent import BaseAgent, AgentWithConverter
+from grid2op.Reward import GameplayReward, L2RPNReward, FlatReward
+from grid2op.Converter import ToVect
+import grid2op
+
+use_cuda = torch.cuda.is_available()
+device   = torch.device("cuda" if use_cuda else "cpu")
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, obs, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (obs, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        obs, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return obs, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class NormalizedActions(AgentWithConverter):
+    def __init__(self, env, observation_space, action_space, args=None):
+        super(NormalizedActions, self).__init__(action_space, action_space_converter=ToVect)
+        self.__bias = 0.0
+        self.env = env
+        self.obs_space = observation_space
+
+        self.obs_size = observation_space.size()
+        self.action_size = action_space.size()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        #print('obs space:{}; action space: {}'.format(self.obs_size, self.action_size))
+
+    def convert_obs(self, observation):
+        """
+        将observation转换成numpy vector的形式，便于处理
+        """
+        obs_vec = observation.to_vect()
+        return obs_vec
+
+    def convert_act(self, encoded_act):
+        """
+        将my_act返回的动作编号转换回env能处理的字典形式
+        """
+
+        res = self.__init__(ToVect.from_vect(encoded_act, check_legit=False))
+        return res
+
+    def my_act(self, transformed_observation, reward, done=False):
+
+
+        encoded_act = self.select_action(transformed_observation)
+        #print(encoded_act)
+        return self.convert_act(encoded_act)
+
+
+    def select_action(self, transformed_obs, test=False):
+
+        action = policy_net.get_action(obs)
+        return action
+
+
+def plot(frame_idx, rewards):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.plot(rewards)
+    plt.show()
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dim, init_w=3e-3):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(state_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+
+class SoftQNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+        super(SoftQNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state, action):
+        x = torch.cat([state, action], 1)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3, log_std_min=-20, log_std_max=2):
+        super(PolicyNetwork, self).__init__()
+
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+
+        self.mean_linear = nn.Linear(hidden_size, num_actions)
+        self.mean_linear.weight.data.uniform_(-init_w, init_w)
+        self.mean_linear.bias.data.uniform_(-init_w, init_w)
+
+        self.log_std_linear = nn.Linear(hidden_size, num_actions)
+        self.log_std_linear.weight.data.uniform_(-init_w, init_w)
+        self.log_std_linear.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+
+        return mean, log_std
+
+    def evaluate(self, state, epsilon=1e-6):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+
+        normal = Normal(mean, std)
+        z = normal.sample()
+        action = torch.tanh(z)
+
+        log_prob = normal.log_prob(z) - torch.log(1 - action.pow(2) + epsilon)
+        log_prob = log_prob.sum(-1, keepdim=True)
+
+        return action, log_prob, z, mean, log_std
+
+    def get_action(self, obs):
+        obs = torch.FloatTensor(obs).unsqueeze(0).to(device)
+        mean, log_std = self.forward(obs)
+        std = log_std.exp()
+
+        normal = Normal(mean, std)
+        z = normal.sample()
+        action = torch.tanh(z)
+
+        action = action.detach().cpu().numpy()
+
+        return action[0]
+
+
+def soft_q_update(batch_size,
+                  gamma=0.99,
+                  mean_lambda=1e-3,
+                  std_lambda=1e-3,
+                  z_lambda=0.0,
+                  soft_tau=1e-2,
+                  ):
+    obs, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    obs = torch.FloatTensor(obs).to(device)
+    next_state = torch.FloatTensor(next_state).to(device)
+    action = torch.FloatTensor(action).to(device)
+    reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+    done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+
+    expected_q_value = soft_q_net(obs, action)
+    expected_value = value_net(obs)
+    new_action, log_prob, z, mean, log_std = policy_net.evaluate(obs)
+
+    target_value = target_value_net(next_state)
+    next_q_value = reward + (1 - done) * gamma * target_value
+    q_value_loss = soft_q_criterion(expected_q_value, next_q_value.detach())
+
+    expected_new_q_value = soft_q_net(obs, new_action)
+    next_value = expected_new_q_value - log_prob
+    value_loss = value_criterion(expected_value, next_value.detach())
+
+    log_prob_target = expected_new_q_value - expected_value
+    policy_loss = (log_prob * (log_prob - log_prob_target).detach()).mean()
+
+    mean_loss = mean_lambda * mean.pow(2).mean()
+    std_loss = std_lambda * log_std.pow(2).mean()
+    z_loss = z_lambda * z.pow(2).sum(1).mean()
+
+    policy_loss += mean_loss + std_loss + z_loss
+
+    soft_q_optimizer.zero_grad()
+    q_value_loss.backward()
+    soft_q_optimizer.step()
+
+    value_optimizer.zero_grad()
+    value_loss.backward()
+    value_optimizer.step()
+
+    policy_optimizer.zero_grad()
+    policy_loss.backward()
+    policy_optimizer.step()
+
+    for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+        target_param.data.copy_(
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
+
+# 指定服务器GPU编号
+
+# create an environment
+env_name = "l2rpn_neurips_2020_track1_small"  # for example, other environments might be usable
+env = grid2op.make(env_name, reward_class=L2RPNReward, other_rewards={"other_reward" : FlatReward})
+# chunk_size = 1024
+# env.chronics_handler.set_chunk_size(chunk_size)
+print('环境 {} 初始化成功'.format(env_name))
+
+my_agent = NormalizedActions(env, env.observation_space, env.action_space)
+
+
+action_dim = my_agent.action_size
+state_dim = my_agent.obs_size
+print('obs space:{}; action space: {}'.format(state_dim, action_dim))
+hidden_dim = 256
+
+value_net = ValueNetwork(state_dim, hidden_dim).to(device)
+target_value_net = ValueNetwork(state_dim, hidden_dim).to(device)
+
+soft_q_net = SoftQNetwork(state_dim, action_dim, hidden_dim).to(device)
+policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+    target_param.data.copy_(param.data)
+
+value_criterion = nn.MSELoss()
+soft_q_criterion = nn.MSELoss()
+
+value_lr = 3e-4
+soft_q_lr = 3e-4
+policy_lr = 3e-4
+
+value_optimizer = optim.Adam(value_net.parameters(), lr=value_lr)
+soft_q_optimizer = optim.Adam(soft_q_net.parameters(), lr=soft_q_lr)
+policy_optimizer = optim.Adam(policy_net.parameters(), lr=policy_lr)
+
+replay_buffer_size = 1000000
+replay_buffer = ReplayBuffer(replay_buffer_size)
+
+max_frames  = 40000
+max_steps   = 500
+frame_idx   = 0
+rewards     = []
+batch_size  = 128
+
+
+while frame_idx < max_frames:
+    #state = env.reset()
+    obs = my_agent.convert_obs(my_agent.env.reset())
+    #print(obs)
+    episode_reward = 0
+
+    for step in range(max_steps):
+
+        #action = policy_net.get_action(obs)
+        action = my_agent.my_act(obs, reward=None)
+        #print(action)
+        next_state, reward, done, info = env.step(action)
+
+
+
+        replay_buffer.push(obs, action, reward, next_state, done)
+        if len(replay_buffer) > batch_size:
+            soft_q_update(batch_size)
+
+        obs = next_state
+        episode_reward += reward
+        frame_idx += 1
+
+        if frame_idx % 1000 == 0:
+            plot(frame_idx, rewards)
+
+        if done:
+            break
+
+    rewards.append(episode_reward)
