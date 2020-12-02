3a695f3834ed718e8e7804826cc5ef19db93d387
lujinxuan-fang <57823348+lujinxuan-fang@users.noreply.github.com>
2020-12-02 11:54:38 +0800

    Update SACdiscGrid.py

--- SACdiscGrid.py
+++ SACdiscGrid.py
@@ -96,58 +96,6 @@ class Critic(nn.Module):
 
 
 class Q(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Q, self).__init__()
-        self.fc1 = nn.Linear(state_dim + action_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
-
-    def forward(self, s, a):
-        s = s.reshape(-1, state_dim)
-        a = a.reshape(-1, action_dim)
-        x = torch.cat((s, a), -1) # combination s and a
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-class Actor(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
-
-        self.log_std_head = nn.Linear(256, action_dim)
-
-
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        #mu = self.mu_head(x)
-        #log_std_head = F.relu(self.log_std_head(x))
-        #log_std_head = torch.clamp(log_std_head, self.min_log_std, self.max_log_std)
-        log_std_head = F.softmax(self.log_std_head(x), dim=1)
-
-        return log_std_head
-
-
-class Critic(nn.Module):
-    def __init__(self, state_dim):
-        super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-class Q(nn.Module):
     def __init__(self, state_dim):
         super(Q, self).__init__()
         self.fc1 = nn.Linear(state_dim, 256)
@@ -155,25 +103,29 @@ class Q(nn.Module):
         self.fc3 = nn.Linear(256, 1)
 
     def forward(self, s, a):
-        s = s.reshape(-1, state_dim)
+        x = s.reshape(-1, state_dim)
         #a = a.reshape(-1, action_dim)
         #x = torch.cat((s, a), -1) # combination s and a
-        x = F.relu(self.fc1(s))
-        x = F.relu(self.fc2(s))
-        x = self.fc3(s)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
-
 class SAC(object):
-    def __init__(self, state_dim, action_dim, Transition):
+    def __init__(self, state_dim, action_dim):
         super(SAC, self).__init__()
 
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # initialize networks
         self.policy_net = Actor(state_dim, action_dim).to(device)
         self.value_net = Critic(state_dim).to(device)
         self.Q_net = Q(state_dim).to(device)
         self.Target_value_net = Critic(state_dim).to(device)
-
+        
+        # Initialize the buffer
         self.replay_buffer = [Transition] * args.capacity
+        
+        # Initialize the optimizer
         self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=args.learning_rate)
         self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=args.learning_rate)
         self.Q_optimizer = optim.Adam(self.Q_net.parameters(), lr=args.learning_rate)
@@ -304,7 +256,7 @@ class SAC(object):
 
 
 class SACagent(AgentWithConverter):
-    def __init__(self, env, observation_space, action_space,Transition, args=None):
+    def __init__(self, env, observation_space, action_space, args=None):
         super(SACagent, self).__init__(action_space, action_space_converter=IdToAct)
         self.env = env
         self.obs_space = observation_space
@@ -351,12 +303,12 @@ class SACagent(AgentWithConverter):
         根据obs返回encoded action
         此时的action已取过item()，即单个int类型数据
         """
-        action, action_prob = self.ppo.select_action(transformed_obs)
+        action, action_prob = self.SAC.select_action(transformed_obs)
 
         return action, action_prob
 
-    def store_transition(self, transition):
-        self.SAC.store(transition)
+    def store_transition(self,state, action, reward, next_state, done):
+        self.SAC.store(state, action, reward, next_state, done)
 
 
 def main():
@@ -382,7 +334,7 @@ def main():
             ep_r += reward
 
             if args.render: env.render()
-            agent.store(state, action, reward, next_state, done)
+            agent.store_transition(state, action, reward, next_state, done)
 
             if agent.num_transition >= args.capacity:
                 agent.update()
